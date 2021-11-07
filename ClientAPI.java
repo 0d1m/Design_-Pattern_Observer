c6c47bc83e8d5637a7fdf1d71f55aeb431d7ed23
medodim <mmbatrone@gmail.com>
2021-11-07 18:09:34 +0100

    API commit

---
+++ ClientAPI.java
@@ -0,0 +1,17 @@
+package Java_API;
+
+import java.util.Observer;
+
+public class ClientAPI {
+
+	public static void main(String[] args) {
+		Subject subject = new Subject(0);
+		Observer binary = new BinaryObserver(subject);
+		Observer octal = new OctalObserver(subject);
+		Observer hexa = new HexaObserver(subject);
+		Thread thread = new Thread(subject);
+		thread.start();
+
+	}
+
+}
